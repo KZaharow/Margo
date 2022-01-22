d11502a1ec295f9184eb5721421b8ef661549440
zaharow <kzaharow@gmail.com>
2022-01-23 01:20:22 +0300

    release 2

--- src/main/java/com/zahar/margarita/controller/DataController.java
+++ src/main/java/com/zahar/margarita/controller/DataController.java
@@ -1,9 +1,10 @@
 package com.zahar.margarita.controller;
 
 import com.zahar.margarita.service.DataService;
-import com.zahar.margarita.service.DataServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,18 @@ import java.time.LocalDateTime;
 public class DataController {
 
     private final DataService dataService;
-    @GetMapping
-    public String getData(){
 
-        //return dataService.createPerson().toString();
-        //return dataService.readPerson(LocalDateTime.now()).toString();
-        //return dataService.readAllPerson().toString();
-        return dataService.readAllPersonInDateRange(LocalDateTime.now().minusDays(1000),LocalDateTime.now())
+    @GetMapping("/{start}/{monthDeep}")
+    public String getData(
+            @PathVariable("start") @DateTimeFormat(pattern = "dd.MM.yy kk:mm:ss,SSSSSS") LocalDateTime start,
+            @PathVariable("monthDeep") long deepMonth
+    ) {
+        return dataService.readAllPersonInDateRange(start, start.plusMonths(deepMonth))
                 .toString();
+    }
+
+    @GetMapping("/all")
+    public String getAllData() {
+        return dataService.readAllPerson().toString();
     }
 }
