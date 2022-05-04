a47a87ca7f660ddb41d3256061aed3968a3b7e34
zaharow <kzaharow@gmail.com>
2022-05-04 22:14:02 +0300

    view 6

--- src/main/java/com/zahar/margarita/controller/PersonController.java
+++ src/main/java/com/zahar/margarita/controller/PersonController.java
@@ -5,12 +5,15 @@ import com.zahar.margarita.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/forms")
+@RequestMapping("/person")
 public class PersonController {
 
     @Autowired
