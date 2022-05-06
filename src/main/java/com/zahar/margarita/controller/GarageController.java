4bf1c8195f9031cf30aa577c2f59d318ae1bdc0b
zaharov <KZaharow@gmail.com>
2022-05-06 17:31:41 +0300

    news-update 7

--- src/main/java/com/zahar/margarita/controller/GarageController.java
+++ src/main/java/com/zahar/margarita/controller/GarageController.java
@@ -1,24 +1,22 @@
 package com.zahar.margarita.controller;
 
 import com.zahar.margarita.entity.Garage;
-import com.zahar.margarita.entity.News;
+import com.zahar.margarita.entity.Person;
 import com.zahar.margarita.service.GarageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Min;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 @RequestMapping("/garage")
+@RequiredArgsConstructor
 public class GarageController {
 
-    @Autowired
-    GarageService garageService;
+    private final GarageService garageService;
 
     @GetMapping("")
     public String getAllGarages(Model model) {
@@ -36,12 +34,13 @@ public class GarageController {
 
     @PostMapping("/update")
     public String putNews(@RequestParam Long id,
-                          @RequestParam String header,
-                          @RequestParam String text,
-                          @RequestParam(defaultValue = "") String isStatus,
-                          @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date) {
-        News news = new News(id, date, header, text, !isStatus.equals(""));
-        newsService.updateNews(news);
-        return "redirect:/news";
+                          @RequestParam Long number,
+                          @RequestParam String description,
+                          @RequestParam Person owner,
+                          @RequestParam Double square,
+                          @RequestParam(defaultValue = "") String isPrivate) {
+        Garage garage = new Garage(id, number, owner, !isPrivate.equals(""), square, description);
+        garageService.updateGarage(garage);
+        return "redirect:/garage/";
     }
 }
